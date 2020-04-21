80b860a9a43287e11b7232fb2ce01db208a2db8c
Samuel Guevara <samsjamin@outlook.com>
2020-04-20 22:42:46 -0400

    finished creating all user questions.

--- Develop/index.js
+++ Develop/index.js
@@ -7,13 +7,13 @@ const writeFileAsync = util.promisify(fs.writeFile);
 inquirer.prompt([
     {
         type: "input",
-        name: "email",
-        message: "Pleaes enter your email: "
+        name: "gitUsername",
+        message: "Please enter your GitHub username: "
     },
     {
         type: "input",
-        name: "gitUsername",
-        message: "Please enter your GitHub username: "
+        name: "Please enter the project URL (including https:// ): ",
+        message: ""
     },
     
     {
@@ -24,6 +24,29 @@ inquirer.prompt([
     {
         type: "input",
         name: "projDescription",
-        message: ""
+        message: "Please describe your project: "
     },
-])
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please enter guidlines for developers contributing to this project: "
+    },
+    {
+        type: "list",
+        name: "licensing",
+        message: "Please select which liscense your project should have: ",
+        choices: ["APACHE 2.0", "GNU AGPLv3", "MIT","Mozilla Public 2.0","NONE"]
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "Please enter the command for running tests to your repo: "
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Pleaes enter an email for user questions: "
+    }
+]).then(function(answers){
+    
+})
