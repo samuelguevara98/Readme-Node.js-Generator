53df1ed59f849e5071e4ffaec940c1b7f0c65f44
Samuel Guevara <samsjamin@outlook.com>
2020-04-20 22:12:16 -0400

    added consts and prompt questions to my index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,29 @@
-const questions = [
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
-
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
+inquirer.prompt([
+    {
+        type: "input",
+        name: "email",
+        message: "Pleaes enter your email: "
+    },
+    {
+        type: "input",
+        name: "gitUsername",
+        message: "Please enter your GitHub username: "
+    },
+    
+    {
+        type: "input",
+        name: "projTitle",
+        message: "Please enter the title of your project: "
+    },
+    {
+        type: "input",
+        name: "projDescription",
+        message: ""
+    },
+])
